5b707f3c926f203e7c0b157ea52ec4754524fb45
Gabriel Wisky Benatti <gabrielwbenatti.dev@gmail.com>
2025-10-15 14:32:55 -0300

    chore: start Spring Boot project

---
+++ api/src/main/java/com/gabrielwbenattidev/carwashx/CarwashxApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielwbenattidev.carwashx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarwashxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarwashxApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/gabrielwbenattidev/carwashx/CarwashxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielwbenattidev.carwashx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarwashxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
